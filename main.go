daf2c0fdd7af4685bffc1907450da8ef8811256b
forDecember <1921567337@qq.com>
2023-12-03 19:22:04 +0800

    update: 展示

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	start := [3][3]int{
@@ -11,6 +13,20 @@ func main() {
 		{1, 2, 3},
 		{8, 0, 4},
 		{7, 6, 5},
+	}
+	println("start -> target")
+	for i := 0; i < 3; i++ {
+		startStr := ""
+		targetStr := ""
+		for j := 0; j < 3; j++ {
+			startStr += fmt.Sprintf("%d  ", start[i][j])
+			targetStr += fmt.Sprintf("%d  ", start[i][j])
+		}
+		if i == 1 {
+			println(startStr + "    =>    " + targetStr)
+		} else {
+			println(startStr + "          " + targetStr)
+		}
 	}
 
 	println("######## =BFS= ########")
